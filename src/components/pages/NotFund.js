0a5e02be81cacbece3dea5259aef30d6479ea666
kaku-kuo <z22857744@gmail.com>
2020-03-28 19:04:59 +0800

    ready to depoly

---
+++ src/components/pages/NotFund.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFund = () => {
+    return (
+        <div>
+           <h1>Not Fund</h1>
+           <p className='lead'>The page you are looking for does not exist...</p> 
+        </div>
+    );
+};
+
+export default NotFund
